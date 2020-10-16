db1e9682b7e74147fe80342371ab6846ced2d5a6
Michael Peter Gudz <michaelp.gudz@gmail.com>
2020-10-16 22:52:15 +0200

    Update Python Launcher location

--- Launch_backup.bat
+++ Launch_backup.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Backup scripts launched in:
 timeout /T 5
-"C:\Program Files (x86)\Python38-32\python.exe" "C:\Users\Michael\OneDrive - Jacobs\Python Project\Backup Scripts\Keypass_backup.py"
-"C:\Program Files (x86)\Python38-32\python.exe" "C:\Users\Michael\OneDrive - Jacobs\Python Project\Backup Scripts\MMEX_DB_Backup.py"
+"C:\Program Files\Python38\python.exe" "C:\Users\Michael\OneDrive - Jacobs\Python Project\Backup Scripts\Keypass_backup.py"
+"C:\Program Files\Python38\python.exe" "C:\Users\Michael\OneDrive - Jacobs\Python Project\Backup Scripts\MMEX_DB_Backup.py"
 pause
